8dc431e5f9be18ed2af8814c43551b6c2a0e38d3
Veit Hueter <info@pinkblau.de>
2022-09-30 15:38:24 +0200

    Media Queriesin html-css

---
+++ week1/day4/exercises.js
@@ -0,0 +1,21 @@
+// // exercise 2
+
+// function reverse(anyAarray) {
+//     let workingArray = anyAarray;
+//     if (Array.isArray(workingArray)) {
+//         let outputArray = [];
+//         workingArray.forEach((element) => outputArray.unshift(element));
+//         return outputArray;
+//     } else {
+//         console.log("Not an Array!");
+//     }
+// }
+
+// let test = reverse([1, 2, 3]);
+
+// console.log(test);
+
+function getNegative(anyOtherAarray) {
+    let arrayToFilter = anyOtherAarray;
+    arrayToFilter.filter((element) => element);
+}
