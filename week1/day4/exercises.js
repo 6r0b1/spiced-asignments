604f24f937437b145d1c3449f93fdf3583d75e46
Veit Hueter <info@pinkblau.de>
2022-09-30 16:49:00 +0200

    vertical align top

--- week1/day4/exercises.js
+++ week1/day4/exercises.js
@@ -1,21 +1,42 @@
-// // exercise 2
+// exercise 1
 
-// function reverse(anyAarray) {
-//     let workingArray = anyAarray;
-//     if (Array.isArray(workingArray)) {
-//         let outputArray = [];
-//         workingArray.forEach((element) => outputArray.unshift(element));
-//         return outputArray;
-//     } else {
-//         console.log("Not an Array!");
-//     }
-// }
+let testCase;
 
-// let test = reverse([1, 2, 3]);
-
-// console.log(test);
-
-function getNegative(anyOtherAarray) {
-    let arrayToFilter = anyOtherAarray;
-    arrayToFilter.filter((element) => element);
+function each(objectOrArray, someCallback) {
+    testCase = objectOrArray;
+    console.log(testCase);
+    if (Array.isArray(testCase)) {
+        testCase.forEach((element) => someCallback(testCase.indexOf(element)));
+    }
 }
+
+each([3, 4], function (sayThis) {
+    console.log(testCase[sayThis]);
+});
+
+// exercise 2
+
+function reverse(anyAarray) {
+    let workingArray = anyAarray;
+    if (Array.isArray(workingArray)) {
+        let outputArray = [];
+        workingArray.forEach((element) => outputArray.unshift(element));
+        return outputArray;
+    } else {
+        console.log("Not an Array!");
+    }
+}
+
+let testexercise1 = reverse([1, 2, 3]);
+
+console.log(testexercise1);
+
+// exercise 3
+
+function getLessThanZero(anyOtherArray) {
+    return anyOtherArray.filter((item) => item < 0);
+}
+
+let testexercise2 = getLessThanZero([3, -2, -3]);
+
+console.log(testexercise2);
