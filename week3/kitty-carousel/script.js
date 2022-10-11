2f74b9ccefe9d0675b1abc5fca289c41e4bab323
Veit Hueter <info@pinkblau.de>
2022-10-11 11:39:51 +0200

    initial klickable indicator dots

--- week3/kitty-carousel/script.js
+++ week3/kitty-carousel/script.js
@@ -11,11 +11,11 @@
         imageDivs[kittyCount].classList.remove("carousel_onscreen");
         imageDivs[kittyCount].classList.add("carousel_stage_left");
         dotIndicators[kittyCount].classList.remove("active_dot");
-        stageLeft = document.querySelector(".carousel_stage_left");
 
         // Increase count to address next image
         kittyCount++;
 
+        stageLeft = document.querySelector(".carousel_stage_left");
         // reset count when end of images list is reached
         // must be done before addressing next in line
         // otherwise uncaught at index 4
@@ -31,19 +31,34 @@
 
         // finally addressing next in line and calling back w/ delay
         imageDivs[kittyCount].classList.add("carousel_onscreen");
+
+        // Would like to have the dot show up a little before transitionend
+        // ... put it outside the listener then
+        dotIndicators[kittyCount].classList.add("active_dot");
         setTimeout(moveCarousel, 3000);
 
         stageLeft.addEventListener("transitionend", function () {
             // listener is only on images, not on dots
-            let stageLeft = document.querySelector(".carousel_stage_left");
             stageLeft.classList.remove("carousel_stage_left");
-            dotIndicators[kittyCount].classList.add("active_dot"); // Would like to have the dot show up a little before transitionend ...
         });
         // wierd stuff happens when changing tab
         // ask about it
     }
 
     setTimeout(moveCarousel, 1000);
+
+    // Add on click to dotIndicators, use for loop
+
+    for (let i = 0; i < dotIndicators.length; i++) {
+        dotIndicators[i].addEventListener("click", function () {
+            imageDivs[kittyCount].classList.remove("carousel_onscreen");
+            imageDivs[kittyCount].classList.add("carousel_stage_left");
+            dotIndicators[kittyCount].classList.remove("active_dot");
+            kittyCount = i;
+            imageDivs[kittyCount].classList.add("carousel_onscreen");
+            dotIndicators[kittyCount].classList.add("active_dot");
+        });
+    }
 })();
 
 // Add and remove bacground to the indicator much the same way
