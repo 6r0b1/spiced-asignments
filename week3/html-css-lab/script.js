da815af590bf6c2c816ecebee4195da3a5b39062
Veit Hueter <info@pinkblau.de>
2022-10-13 10:20:27 +0200

    modal vanilla JS

---
+++ week3/html-css-lab/script.js
@@ -0,0 +1,41 @@
+(function () {
+    // burger menu
+
+    let menuBox = document.getElementsByClassName("menu_slide")[0];
+    let backSoft = document.getElementsByClassName("soften")[0];
+    let everywhere = document.getElementsByTagName("main")[0];
+    let hamburger = document.getElementsByClassName("menu")[0];
+    let fade = document.getElementById("menu_img");
+    console.log(hamburger);
+
+    hamburger.addEventListener("click", function (e) {
+        menuBox.classList.toggle("slide_in_right");
+        backSoft.classList.toggle("fade_in_black");
+        fade.classList.toggle("hide_menu_image");
+        e.stopPropagation();
+    });
+
+    menuBox.addEventListener("klick", function (e) {
+        e.stopPropagation();
+    });
+
+    everywhere.addEventListener("click", function () {
+        menuBox.classList.remove("slide_in_right");
+        backSoft.classList.remove("fade_in_black");
+    });
+
+    // disclaimer
+
+    let disclaimerContainer = document.querySelector(".disclaimer_invisible");
+    let closeButton = document.querySelector(".close_disclaimer");
+
+    setTimeout(function () {
+        disclaimerContainer.classList.remove("disclaimer_invisible");
+        disclaimerContainer.classList.add("disclaimer_visible");
+    }, 1000);
+
+    closeButton.addEventListener("click", function () {
+        disclaimerContainer.classList.remove("disclaimer_visible");
+        disclaimerContainer.classList.add("disclaimer_invisible");
+    });
+})();
