144c6cb868792d5f81a3bb3621f9a9ceb12eb796
Veit Hueter <info@pinkblau.de>
2022-10-26 15:42:10 +0200

    colorful console added commentary

--- week5/colorful-console/index.js
+++ week5/colorful-console/index.js
@@ -3,12 +3,15 @@ const fs = require("fs");
 const chalk = require("chalk");
 
 http.createServer((request, response) => {
+    // Store properties of request for later use
     const { headers, method, url } = request;
 
+    // log request to text file
     let date = new Date();
-    let logEntry = `${date}\t${request.method}\t${request.url}\t${request.headers["user-agent"]}\n`;
+    let logEntry = `${date}\t|\tMethod: ${request.method}\tURL: ${request.url}\tUser-Agent: ${request.headers["user-agent"]}\n`;
     fs.appendFile("requests.txt", logEntry, () => {});
 
+    // Standard resposnse to get/head with hard coded html
     if (request.method === "GET" || request.method === "HEAD") {
         response.writeHead(200, { "Content-Type": "text/html" });
         if (request.method === "HEAD") {
@@ -18,29 +21,35 @@ http.createServer((request, response) => {
             '<!doctype html><html><title>Colors</title><form method="POST"><input type="text" name="text"><select name="color"><option value="red">red</option><option value="blue">blue</option><option value="green">green</option><option value="yellow">yellow</option><option value="gray">gray</option><option value="magenta">magenta</option><option value="cyan">cyan</option></select><button type="submit">Go</button></form></html>';
         response.end(responseBody);
     }
+
+    // Response to post from submitted serch/form
     if (request.method === "POST") {
         let body = [];
+        // search parameters
         let text;
         let color;
         request
             .on("error", (err) => {
                 console.error(err);
             })
+            // construct request body
             .on("data", (chunk) => {
                 body.push(chunk);
             })
+            // Generate search query
             .on("end", () => {
                 body = Buffer.concat(body).toString();
                 const query = new URLSearchParams(body);
                 text = query.get("text");
                 color = query.get("color");
-
+                // Respond with html and variables from query
                 response.writeHead(200, { "Content-Type": "text/html" });
                 response.end(
                     `<!doctype html><html><title>colorful-console</title><a href="/" style="color:${color}">${text}</a></html>`
                 );
                 console.log(chalk[color](text));
             });
+        // Respond to forbidden methods
     } else {
         response.writeHead(
             405,
